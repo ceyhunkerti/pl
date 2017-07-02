18ca78b04c715c489c9ed3df95981bd7ea65f0a3
Ceyhun Kerti <ceyhun.kerti@bluecolor.io>
2017-07-02 19:22:16 +0300

    pl spec

--- src/pl/pl.pks.sql
+++ src/pl/pl.pks.sql
@@ -6,10 +6,9 @@ as
   
   procedure truncate_table(piv_owner varchar2, piv_table varchar2);
   procedure drop_table(piv_owner varchar2, piv_table varchar2, pib_ignore_err boolean default true);
-  function table_exists(piv_owner in varchar2, piv_table in varchar2) return boolean;
+  function  table_exists(piv_owner in varchar2, piv_table in varchar2) return boolean;
   procedure enable_parallel_dml;
   procedure disable_parallel_dml;
-
 
   procedure printl(piv_message varchar2);
   procedure print (piv_message varchar2);
